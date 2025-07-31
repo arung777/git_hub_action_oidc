2b027537caa0f77fea44a37671b4e5938a9d64ee
charan <arunrenaldo7@gmail.com>
2025-07-31 11:19:43 +0530

    this is commit for destroying 2

--- non-prod/dev_env/aws_resources/VPC.tf
+++ non-prod/dev_env/aws_resources/VPC.tf
@@ -1,26 +1,26 @@
-module "myvpc" {
-    source = "../../../modules/AWS_vpc"
+# module "myvpc" {
+#     source = "../../../modules/AWS_vpc"
 
-    aws_region          = var.aws_region
-    vpc_tag_name        = var.vpc_tag_name
-    public_subnet_tag   = var.public_subnet_tag
-    route_cidr_block    = var.route_cidr_block
-    private_subnet_tag  = var.private_subnet_tag
-    aws_access_key      = var.aws_access_key
-    secret_key          = var.secret_key
-    cidr_block          = var.cidr_block
-    public_subnet_cidr  = var.public_subnet_cidr
-    private_subnet_cidr = var.private_subnet_cidr
+#     aws_region          = var.aws_region
+#     vpc_tag_name        = var.vpc_tag_name
+#     public_subnet_tag   = var.public_subnet_tag
+#     route_cidr_block    = var.route_cidr_block
+#     private_subnet_tag  = var.private_subnet_tag
+#     aws_access_key      = var.aws_access_key
+#     secret_key          = var.secret_key
+#     cidr_block          = var.cidr_block
+#     public_subnet_cidr  = var.public_subnet_cidr
+#     private_subnet_cidr = var.private_subnet_cidr
     
-}
+# }
 
-output "vpc_cidr_block" {
-    value       = module.myvpc.vpc_cidr_block
-    description = "CIDR block of the created VPC"
-}
+# output "vpc_cidr_block" {
+#     value       = module.myvpc.vpc_cidr_block
+#     description = "CIDR block of the created VPC"
+# }
 
-output "vpc_id" {
-    value       = module.myvpc.vpc_id
-    description = "ID of the created VPC"
-}
+# output "vpc_id" {
+#     value       = module.myvpc.vpc_id
+#     description = "ID of the created VPC"
+# }
 
