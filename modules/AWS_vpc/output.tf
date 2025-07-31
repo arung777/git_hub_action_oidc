5e5681887d6e0afb862d7f39855046cfd249e916
charan <arunrenaldo7@gmail.com>
2025-07-31 10:28:35 +0530

    added output for vpc_id to display that

--- modules/AWS_vpc/output.tf
+++ modules/AWS_vpc/output.tf
@@ -2,3 +2,8 @@ output "vpc_cidr_block" {
   value = data.aws_vpc.myvpc.cidr_block
   description = "CIDR block of the VPC fetched from the data source"
 }
+
+output "vpc_id" {
+  value = data.aws_vpc.myvpc.id
+  description = "ID of the VPC fetched from the data source"
+}
