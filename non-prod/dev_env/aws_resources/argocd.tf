79b1907c255969e50abf4ab6ef4d219533057696
charan <arunrenaldo7@gmail.com>
2025-08-08 11:58:06 +0530

    added some steps to the workflow file

--- non-prod/dev_env/aws_resources/argocd.tf
+++ non-prod/dev_env/aws_resources/argocd.tf
@@ -28,9 +28,9 @@ provider "kubernetes" {
 
 # 3. Helm provider to install Helm charts into the Kubernetes cluster
 provider "helm" {
-    kubernetes =  {
-        config_path = "~/.kube/config"
-    }
+    # kubernetes =  {
+    #     config_path = "~/.kube/config"
+    # }
 }
 
 # 4. Helm release resource to install Argo CD
